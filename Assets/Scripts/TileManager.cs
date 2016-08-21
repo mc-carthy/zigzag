06b28aa580d9e8476e46dec432110428e11a3396
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-21 09:21:13 +0100

    Random platform path generation

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject platform;
+	public int numOfPlatforms = 20;
+
+	private GameObject lastPlatform;
+
+	private void Start () {
+		lastPlatform = Instantiate (platform) as GameObject;
+		lastPlatform.transform.SetParent (this.transform);
+
+		for (int i = 0; i < numOfPlatforms; i++) {
+			SpawnPlatform ();
+		}
+	}
+
+	public void SpawnPlatform () {
+		int random = Random.Range (0, 2);
+
+		if (random == 0) {
+			lastPlatform = Instantiate (platform, new Vector3 (lastPlatform.transform.position.x - 1, lastPlatform.transform.position.y, lastPlatform.transform.position.z), Quaternion.identity) as GameObject;
+		} else {
+			lastPlatform = Instantiate (platform, new Vector3 (lastPlatform.transform.position.x, lastPlatform.transform.position.y, lastPlatform.transform.position.z + 1), Quaternion.identity) as GameObject;
+		}
+		lastPlatform.transform.SetParent (this.transform);
+	}
+}
