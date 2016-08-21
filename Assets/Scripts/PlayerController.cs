9c2156a16d2040be0fc05ec0d21c9e612e7fd37e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-21 09:06:45 +0100

    Generate player controller & control mechanism

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum State { left, up, down, right };
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private State state = State.up;
+	private Rigidbody rb;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			switch (state) {
+			case State.left:
+				state = State.up;
+				rb.velocity = new Vector3 (-speed, 0f, 0f);
+				break;
+			case State.up:
+				state = State.left;
+				rb.velocity = new Vector3 (0f, 0f, speed);
+				break;
+			}
+		}
+	}
+	
+}
